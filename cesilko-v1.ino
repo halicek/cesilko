9114d6e790da331cc120c3ff04cb57b7fd5f043f
halicek <josef.halicek@gmail.com>
2017-08-29 00:11:45 +0200

    First running version

--- cesilko-v1.ino
+++ cesilko-v1.ino
@@ -1,14 +1,15 @@
-/*Liquid crystal control code based on Arduino tutorials:
-  https://www.arduino.cc/en/Tutorial/HelloWorld 
- */
+/* Big portion of code base on Arduino tutorial codes (LCD crystal, rotary encoder, EEPROM read and write).
+ * TODO: Set backlight to be controlled by the program (via PWM on LCD pin A) -- not going to happen :/ (maybe ver 2 on another pcb)
+*/
 
-//TODO: Set backlight to be controlled by the program (via PWM on LCD pin A.
 
 #include <LiquidCrystal.h>
+#include <EEPROM.h>
 
 // pin numbers
-#define switch_black 6
-#define button_up 7
+// TODO: UPDATE
+#define switch_black A3
+#define button_up 8
 #define button_down 8
 #define button_red 9
 #define relay 10
@@ -18,14 +19,15 @@
 #define S_NORMAL 0
 #define S_AUTOPUMP 1
 #define S_MANPUMP 2
-#define S_SET_ALARM_H 3
-#define S_SET_ALARM_M 4
-#define S_SET_PUMP_H 5
-#define S_SET_PUMP_M 6
+#define S_SET_PUMP_M 3
+#define S_SET_PUMP_S 4
+#define S_SET_ALARM_H 5
+#define S_SET_ALARM_M 6
 
 // states
 uint8_t status = 0; // FSM status variable, 0 = default (automatic countdown mode)
-uint8_t switch_black_state = 0;
+// TODO: FIX THIS (switch black state not exicitng)
+uint8_t switch_black_state = HIGH;
 uint8_t button_up_state = 0; 
 uint8_t button_down_state = 0;
 uint8_t button_red_state = 0;
@@ -36,16 +38,15 @@ uint8_t bell[8]  = {0x4,0xe,0xe,0xe,0x1f,0x0,0x4};
 uint8_t clock[8] = {0x0,0xe,0x15,0x17,0x11,0xe,0x0};
 uint8_t duck[8]  = {0x0,0xc,0x1d,0xf,0xf,0x6,0x0};
 
-
 // initializing LCD 1602A with Hitachi HD44780 driver
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 12, en = 11, d4 = 2, d5 = 3, d6 = 4, d7 = 5;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // default user time values - everything in SECONDS
 //TODO: Set reasonable initial times
 long running_time = 0L; // how far we are in the current interval
-long alarm_time = 15L; //when to start the pump - how long the interval is
-long pumping_time = 5L; // how long should pump run
+long alarm_time = 120L; //when to start the pump - how long the interval is
+long pumping_time = 20L; // how long should pump run
 long pumping_time_remaining = 0L; // how much time of pumping remains
 long manual_time = 0L; // how long does pump run on manual override
 
@@ -54,6 +55,23 @@ const long freq_sec = 1000;
 const long freq_min = 60000;
 unsigned long previousMillis = 0; 
 
+
+// rotary encoder variables
+int val;
+int encoderPinA = 7;
+int encoderPinB = 9;
+int encoderPinPush = A1; 
+int encoderPos=0;
+int encoderPinALast = HIGH;
+int statusA = LOW;
+int statusB = LOW;
+
+// writing to EEPROM
+int lastpush=0;
+int eepromAddr=0;
+
+// to mark time when the push of the button started.
+unsigned long pushStartMillis = 0L;
 
 
 void setup() {
@@ -73,18 +91,14 @@ void setup() {
   pinMode(button_red, INPUT_PULLUP);
   pinMode(switch_black, INPUT_PULLUP);
   pinMode(relay, OUTPUT);
-  
-  
-}
+  pinMode(encoderPinA, INPUT_PULLUP);
+  pinMode(encoderPinB, INPUT_PULLUP);
+  pinMode(encoderPinPush, INPUT_PULLUP);
 
-/*
-void formatTimeDigits(char strOut[3], int num) // https://stackoverflow.com/questions/1787106/arduino-string-formatting-issue
-{
-  strOut[0] = '0' + (num / 10);
-  strOut[1] = '0' + (num % 10);
-  strOut[2] = '\0';
+  alarm_time = EEPROMReadlong(eepromAddr);
+  pumping_time = EEPROMReadlong(eepromAddr+4);
+  
 }
-*/
 
 /* Function to format time into string like " 4h22m" or "12h05m"
  * output - string (pointer to string) with formatted output
@@ -167,7 +181,7 @@ void printStatus(int status){
     lcd.write((byte)1); // clock
     lcd.setCursor(10,0);
     char running[7];
-    formatTimeM(running,running_time);
+    formatTimeH(running,running_time);
     lcd.print(running);
     lcd.setCursor(0,1);
     lcd.write((byte)2);//duck
@@ -182,7 +196,7 @@ void printStatus(int status){
     lcd.write((byte)0);
     lcd.setCursor(10,1);
     char alarm[7];
-    formatTimeM(alarm,alarm_time);
+    formatTimeH(alarm,alarm_time);
     lcd.print(alarm);
   } 
   else if (status==S_AUTOPUMP){
@@ -209,37 +223,82 @@ void printStatus(int status){
   }  
 }
 
+
+//This function will write a 4 byte (32bit) long to the eeprom at
+//the specified address to address + 3.
+void EEPROMWritelong(int address, long value)
+      {
+      //Decomposition from a long to 4 bytes by using bitshift.
+      //One = Most significant -> Four = Least significant byte
+      byte four = (value & 0xFF);
+      byte three = ((value >> 8) & 0xFF);
+      byte two = ((value >> 16) & 0xFF);
+      byte one = ((value >> 24) & 0xFF);
+
+      //Write the 4 bytes into the eeprom memory.
+      EEPROM.write(address, four);
+      EEPROM.write(address + 1, three);
+      EEPROM.write(address + 2, two);
+      EEPROM.write(address + 3, one);
+}
+
+long EEPROMReadlong(long address)
+      {
+      //Read the 4 bytes from the eeprom memory.
+      long four = EEPROM.read(address);
+      long three = EEPROM.read(address + 1);
+      long two = EEPROM.read(address + 2);
+      long one = EEPROM.read(address + 3);
+
+      //Return the recomposed long by using bitshift.
+      return ((four << 0) & 0xFF) + ((three << 8) & 0xFFFF) + ((two << 16) & 0xFFFFFF) + ((one << 24) & 0xFFFFFFFF);
+      }
+
+
 void loop() {
-  //lcd.setCursor(0, 1);
-  //lcd.print(millis() / 1000);
- 
-
-  // read status TEST
-  button_up_state = digitalRead(button_up);
-  button_down_state = digitalRead(button_down);
-  button_red_state = digitalRead(button_red);
-  switch_black_state = digitalRead(switch_black);
-
-/*
-  // MANUAL TEST
-  if (switch_black_state == LOW) {
-    //digitalWrite(led, HIGH);
-    digitalWrite(relay, HIGH);
-    Serial.print("Yay!");
-  }
-  else{
-    //digitalWrite(led, LOW);
-    digitalWrite(relay, LOW);
-  }
-  // END MANUAL TEST
-*/
    
   unsigned long currentMillis = millis();
 
-  // TODO: check all async state changes here! Setup button, manual override, standard button (if they will get any function later)
+  statusA = digitalRead(encoderPinA);
+  statusB = digitalRead(encoderPinB);
+  int pushed = digitalRead(encoderPinPush);
 
+  if ((lastpush==0)&&(pushed==LOW)){
+      EEPROMWritelong(eepromAddr, alarm_time);
+      EEPROMWritelong(eepromAddr+4, pumping_time);
+      lastpush=1;
+  }
+  if (pushed==HIGH){
+      
+      lastpush=0;
+    }
+    
+  Serial.print(".");
+  if ((encoderPinALast == LOW) && (statusA == HIGH)) {
+    if (pushed==HIGH){
+      if (statusB == LOW) {
+      alarm_time-=300;
+      if (alarm_time<300) alarm_time=300;
+      } else {
+        alarm_time+=300;
+      }
+    }
+    else{
+        if (statusB == LOW) {
+          pumping_time-=10;
+          if (pumping_time<10) pumping_time=10;
+        }
+        else{
+          pumping_time+=10;
+          }
+      }
+  }
+  encoderPinALast = statusA;
+
+  switch_black_state = digitalRead(switch_black);
   // Checking the manual override
   if (switch_black_state == LOW){
+    Serial.println("LOOOOW!");
     if (status!=S_MANPUMP){
       // first time we hit a manual mode
       manual_time=0;
@@ -293,16 +352,4 @@ void loop() {
 
 
       
-  }
-    
-    
-  
-    
-
-  //TODO: remove or change, just to slow down LCD refresh a little
-  //delay(100);      
-}
-    
-
-
-
+  }   
